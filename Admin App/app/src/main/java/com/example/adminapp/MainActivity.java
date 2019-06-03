fdd70cb1b33ca1f7dde0c4994e6dc10c3054fe1b
PaulmarieDjekinnou <paul-marie.djekinnou@etu.unice.fr>
2019-06-03 11:40:40 +0200

    #3
    Affichage des listes d'attentes sans la requête avec le backend

--- Admin App/app/src/main/java/com/example/adminapp/MainActivity.java
+++ Admin App/app/src/main/java/com/example/adminapp/MainActivity.java
@@ -2,12 +2,34 @@ package com.example.adminapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView queues ;
+    private List<String> queuesnames ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        queues = findViewById(R.id.queues);
+        queuesnames = new ArrayList<>();
+        queuesnames.add("BRI");
+        queuesnames.add("Responsable de stage");
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,queuesnames);
+        queues.setAdapter(adapter);
+        queues.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                
+            }
+        });
     }
 }
