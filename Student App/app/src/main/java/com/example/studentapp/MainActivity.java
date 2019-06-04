0fd624648f7f039f438b924021deb55100585e90
Romain Giuntini <romain.giuntini@etu.univ-cotedazur.fr>
2019-06-04 08:51:12 +0200

    #1 Mise en place de la requête Http dans une asynctask (good use)

--- Student App/app/src/main/java/com/example/studentapp/MainActivity.java
+++ Student App/app/src/main/java/com/example/studentapp/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.studentapp;
 
-import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,22 +11,14 @@ import android.widget.TextView;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.example.studentapp.Models.Queue;
 import com.example.studentapp.Models.Ticket;
 import com.example.studentapp.Models.User;
 import com.example.studentapp.Utils.SingletonRequestQueue;
 import com.example.studentapp.Utils.UserLocalStore;
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
-
-import java.lang.reflect.Type;
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,7 +26,6 @@ public class MainActivity extends AppCompatActivity {
     private TextView status;
     private final Gson gson = new Gson();
     private SingletonRequestQueue singleton;
-    private StringRequest stringRequest;
     private User userStored;
 
     @Override
@@ -43,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final UserLocalStore userLocalStore = new UserLocalStore(this);
-        userLocalStore.storeUserData(new User("romain", 1, "Romain", "Giuntini"));
+        userLocalStore.storeUserData(new User("yury", 1, "Yury", "Silvestrov-Henocq"));
         userStored = userLocalStore.getStoredUser();
 
         this.status = (TextView)this.findViewById(R.id.status);
@@ -66,43 +57,48 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void refreshText() {
-        // Instantiate the RequestQueue.
-        singleton = SingletonRequestQueue.getInstance(this);
-        String url = "http://yursilv.alwaysdata.net/api/queues/" + userStored.getAbonnement() + "/currentTicket";
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                // Instantiate the RequestQueue.
+                singleton = SingletonRequestQueue.getInstance(getApplicationContext());
+                String url = "http://yursilv.alwaysdata.net/api/queues/" + userStored.getAbonnement() + "/currentTicket";
 
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                        (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
 
-                    @Override
-                    public void onResponse(JSONObject response) {
+                            @Override
+                            public void onResponse(JSONObject response) {
 
-                        /*Type data_type = new TypeToken<ArrayList<Queue>>(){}.getType();
-                        ArrayList<Queue> data = gson.fromJson(response.toString(), data_type);
-                        StringBuilder str = new StringBuilder();
-                        for (Queue q : data) {
-                            str.append(q.toString() + "\n");
-                        }*/
-                        Ticket currentTicket = gson.fromJson(response.toString(), Ticket.class);
-                        if (currentTicket.getTicketId() == userStored.getAbonnement() && currentTicket.getUserLogin().equals(userStored.getLoggin())) {
-                            status.setText("Oui vous êtes attendu");
-                        } else {
-                            status.setText("Non toujours");
-                        }
-                        Log.d(TAG, currentTicket.toString());
-                        Log.d(TAG, userStored.toString());
-                    }
-                }, new Response.ErrorListener() {
+                                /*Type data_type = new TypeToken<ArrayList<Queue>>(){}.getType();
+                                ArrayList<Queue> data = gson.fromJson(response.toString(), data_type);
+                                StringBuilder str = new StringBuilder();
+                                for (Queue q : data) {
+                                    str.append(q.toString() + "\n");
+                                }*/
+                                Ticket currentTicket = gson.fromJson(response.toString(), Ticket.class);
+                                if (currentTicket.getUserLogin().equals(userStored.getLoggin())) {
+                                    status.setText("Oui vous êtes attendu");
+                                } else {
+                                    status.setText("Non vous n'êtes pas attendu");
+                                }
+                                Log.d(TAG, currentTicket.toString());
+                                Log.d(TAG, userStored.toString());
+                            }
+                        }, new Response.ErrorListener() {
 
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        // TODO: Handle error
-                        status.setText("Oups! ça marche pas très bien...");
-                        Log.d(TAG, error.toString());
-                    }
-                });
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                // TODO: Handle error
+                                status.setText("Oups! ça marche pas très bien...");
+                                Log.d(TAG, error.toString());
+                            }
+                        });
 
-        // Access the RequestQueue through your singleton class.
-        singleton.addToRequestQueue(jsonObjectRequest);
+                // Access the RequestQueue through your singleton class.
+                singleton.addToRequestQueue(jsonObjectRequest);
+            }
+        });
     }
 
 }
