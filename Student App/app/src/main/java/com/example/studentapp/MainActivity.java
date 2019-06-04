264a3d9ff1f8cc7307d516e456c5d1d77dca76ef
Romain Giuntini <romain.giuntini@etu.univ-cotedazur.fr>
2019-06-04 11:28:18 +0200

    #15 Mise en place de l'écoute du backend

--- Student App/app/src/main/java/com/example/studentapp/MainActivity.java
+++ Student App/app/src/main/java/com/example/studentapp/MainActivity.java
@@ -20,7 +20,36 @@ import com.google.gson.Gson;
 
 import org.json.JSONObject;
 
+//import okhttp3.OkHttpClient;
+//import okhttp3.WebSocket;
+//import okhttp3.WebSocketListener;
 public class MainActivity extends AppCompatActivity {
+
+//    private class EchoWebSocketListener extends WebSocketListener {
+//        private static final String TAG = "EchoWebSocketListener";
+//        private static final int NORMAL_CLOSURE_STATUS = 1000;
+//
+//        @Override
+//        public void onOpen(WebSocket webSocket, okhttp3.Response response) {
+//            Log.v(TAG, "WebSocket oppened!");
+//        }
+//
+//        @Override
+//        public void onMessage(WebSocket webSocket, String text) {
+//            refreshText();
+//        }
+//
+//        @Override
+//        public void onClosing(WebSocket webSocket, int code, String reason) {
+//            webSocket.close(NORMAL_CLOSURE_STATUS, null);
+//            Log.v(TAG, "Closing WebSocket: " + code + " / " + reason);
+//        }
+//
+//        @Override
+//        public void onFailure(WebSocket webSocket, Throwable t, okhttp3.Response response) {
+//            Log.e(TAG, "WebSocket error : " + t.getMessage());
+//        }
+//    }
 
     public static final String TAG = "MainActivity";
     private TextView status;
@@ -28,11 +57,16 @@ public class MainActivity extends AppCompatActivity {
     private SingletonRequestQueue singleton;
     private User userStored;
 
+    private Socket socket;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        OkHttpClient client = new OkHttpClient();
+//        okhttp3.Request request = new okhttp3.Request.Builder().url("http://yursilv.alwaysdata.net").build();
+//        EchoWebSocketListener listener = new EchoWebSocketListener();
+//        WebSocket ws = client.newWebSocket(request, listener);
         final UserLocalStore userLocalStore = new UserLocalStore(this);
         userLocalStore.storeUserData(new User("yury", 1, "Yury", "Silvestrov-Henocq"));
         userStored = userLocalStore.getStoredUser();
@@ -101,4 +135,45 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void startWebSocket() {
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    socket = IO.socket("http://yursilv.alwaysdata.net");
+                    socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+
+                        @Override
+                        public void call(Object... args) {
+                            Log.d(TAG, "Conected");
+                            refreshText();
+                        }
+
+                    }).on("update", new Emitter.Listener() {
+
+                        @Override
+                        public void call(Object... args) {
+                            refreshText();
+                        }
+
+                    }).on(Socket.EVENT_DISCONNECT, new Emitter.Listener() {
+
+                        @Override
+                        public void call(Object... args) {
+                            Log.d(TAG, "Disconnected");
+                        }
+
+                    }).on(Socket.EVENT_ERROR, new Emitter.Listener() {
+                        @Override
+                        public void call(Object... args) {
+
+                        }
+                    });
+                    socket.connect();
+                } catch (URISyntaxException e) {
+                    Log.e(TAG, e.getMessage());
+                }
+            }
+        });
+    }
 }
