ebc981fe7ea100504c90437ce43eb8632b819fb8
Romain Giuntini <romain.giuntini@etu.univ-cotedazur.fr>
2019-06-04 11:29:01 +0200

    #5 Mise en place d'une notification quand un étudiant est attendu

--- Student App/app/src/main/java/com/example/studentapp/MainActivity.java
+++ Student App/app/src/main/java/com/example/studentapp/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.studentapp;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.os.AsyncTask;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,9 +25,15 @@ import com.google.gson.Gson;
 
 import org.json.JSONObject;
 
+import java.net.URISyntaxException;
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
 //import okhttp3.OkHttpClient;
 //import okhttp3.WebSocket;
 //import okhttp3.WebSocketListener;
+
 public class MainActivity extends AppCompatActivity {
 
 //    private class EchoWebSocketListener extends WebSocketListener {
@@ -52,21 +63,30 @@ public class MainActivity extends AppCompatActivity {
 //    }
 
     public static final String TAG = "MainActivity";
+    private static final String CHANNEL_ID = "MainActivityNotificationChannel";
+    private static final int NOTIFICATION_ID = 1;
     private TextView status;
     private final Gson gson = new Gson();
     private SingletonRequestQueue singleton;
     private User userStored;
 
     private Socket socket;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        singleton = SingletonRequestQueue.getInstance(this);
+
 //        OkHttpClient client = new OkHttpClient();
 //        okhttp3.Request request = new okhttp3.Request.Builder().url("http://yursilv.alwaysdata.net").build();
 //        EchoWebSocketListener listener = new EchoWebSocketListener();
 //        WebSocket ws = client.newWebSocket(request, listener);
+
+        startWebSocket();
+        createNotificationChannel();
+
         final UserLocalStore userLocalStore = new UserLocalStore(this);
         userLocalStore.storeUserData(new User("yury", 1, "Yury", "Silvestrov-Henocq"));
         userStored = userLocalStore.getStoredUser();
@@ -74,6 +94,7 @@ public class MainActivity extends AppCompatActivity {
         this.status = (TextView)this.findViewById(R.id.status);
         final Button refresh = (Button)this.findViewById(R.id.btn);
 
+        refresh.setVisibility(View.GONE);
         refresh.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -95,7 +116,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 // Instantiate the RequestQueue.
-                singleton = SingletonRequestQueue.getInstance(getApplicationContext());
                 String url = "http://yursilv.alwaysdata.net/api/queues/" + userStored.getAbonnement() + "/currentTicket";
 
                 JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
@@ -103,7 +123,6 @@ public class MainActivity extends AppCompatActivity {
 
                             @Override
                             public void onResponse(JSONObject response) {
-
                                 /*Type data_type = new TypeToken<ArrayList<Queue>>(){}.getType();
                                 ArrayList<Queue> data = gson.fromJson(response.toString(), data_type);
                                 StringBuilder str = new StringBuilder();
@@ -113,6 +132,7 @@ public class MainActivity extends AppCompatActivity {
                                 Ticket currentTicket = gson.fromJson(response.toString(), Ticket.class);
                                 if (currentTicket.getUserLogin().equals(userStored.getLoggin())) {
                                     status.setText("Oui vous êtes attendu");
+                                    showNotification("Vous êtes attendu", "Dépêchez vous!");
                                 } else {
                                     status.setText("Non vous n'êtes pas attendu");
                                 }
@@ -125,7 +145,7 @@ public class MainActivity extends AppCompatActivity {
                             public void onErrorResponse(VolleyError error) {
                                 // TODO: Handle error
                                 status.setText("Oups! ça marche pas très bien...");
-                                Log.d(TAG, error.toString());
+                                Log.e(TAG, error.toString());
                             }
                         });
 
@@ -175,5 +195,32 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Main activity notification channel";
+            String description = "Notification channel of the main activity";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    public void showNotification(String title, String content) {
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder( this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.check)
+                .setContentTitle(title)
+                .setContentText(content)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        Log.d("Main Activity:", "test notification pushed");
+        notificationManagerCompat.notify(NOTIFICATION_ID, builder.build());
     }
 }
--- Student App/app/src/main/java/com/example/studentapp/Utils/PagerAdapter.java
+++ Student App/app/src/main/java/com/example/studentapp/Utils/PagerAdapter.java
@@ -4,7 +4,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 
-import com.example.studentapp.Fragments.QueueFragment;
 import com.example.studentapp.Models.Queue;
 
 import java.util.ArrayList;
